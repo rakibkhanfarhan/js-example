4102c971d11f956772a47b022a49b8a9bf3ce459
Rakib Farhan <167608904+rakibkhanfarhan@users.noreply.github.com>
2024-08-30 00:00:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+//example1
+function textChange(){
+	document.getElementById('tc').innerHTML="welcome to my world";
+}
+//example2
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+
+//example3
+function bulbOn(){
+	document.getElementById('bulb').src='images/on.gif';
+}
+function bulbOff(){
+	document.getElementById('bulb').src='images/off.gif';
+}
+//example4
+function changeCss(){
+	document.getElementById('css').style.color="purple";
+}
+
+//example5
+function textShow(){
+	document.getElementById('csh').style.display="block";
+}
+function textHide(){
+	document.getElementById('csh').style.display="none";
+}
+
+//example6
+function changeFont_size(){
+	document.getElementById('fc').style.fontSize="50px";
+}
+
+//example7
+
+ function smile(){
+	document.getElementById('emoji').src='images/smile.jpg';
+}
+function sad(){
+	document.getElementById('emoji').src='images/sad.jpg';
+}
+
+//example8
+function changeBgc(){
+	document.getElementById('fbcg').style.backgroundColor="teal";
+}
+
+//example9
+
+function picShow(){
+	document.getElementById('sc').style.display= 'block';
+}
+
+function picHide(){
+	
+	document.getElementById('sc').style.display= 'none';
+}
+
+
+
+//example10
+function show(){
+	document.getElementById('scbs').style.backgroundColor='red';
+}
+
+function hide(){
+	
+	document.getElementById('scbs').style.backgroundColor= 'transparent';
+}
+//example11
+function changeBorder(){
+	document.getElementById('cb').style.border= '5px solid black';
+}
+//example12
+function bShow(){
+	document.getElementById('bb').style.border='5px dashed red';
+}
+
+function bHide(){
+	
+	document.getElementById('bb').style.border= 'none';
+}
+//example13
+function alignItem(){
+	document.getElementById('ai').style.textAlign= 'center';
+}
+//example14
+function bcgImg(){
+	document.getElementById('bimg').style.backgroundImage= "url('images/d3.png')";
+}
+//example15
+function ca(){
+	document.getElementById("myDIV").style.animation = "mynewmove 4s 2";
+}
